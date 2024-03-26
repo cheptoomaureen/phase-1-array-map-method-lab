a81be28386e5b13915eca4901e8d8e7ff18fd0a8
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-26 06:40:05 +0300

    assignment

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+function titleCased() {
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
+  'What Does The This Keyword Mean?',
+  'What Is The Constructor OO Pattern?',
+  'Implementing Blockchain Web API',
   'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'What Is NaN And How Can We Check For It',
+  'What Is The Difference Between StopPropagation And PreventDefault?',
+  'Immutable State And Pure Functions',
+  'What Is The Difference Between == And ===?',
+  'What Is The Difference Between Event Capturing And Bubbling?',
+  'What Is JSONP?'
 ];
 
 const titleCased = () => {
   return tutorials
 }
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Use Array.prototype.map() to transform each tutorial title to title case
+return tutorials.map (tutorial => {
+  const words = tutorial.split(' ');
+  const titleCasedWords = words.map(word => capitalizeFirstLetter(word));
+  return titleCasedWords.join(' ');});}
